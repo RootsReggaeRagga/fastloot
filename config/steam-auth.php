d49f70d77f9c8122313c4ce419ded30742a4945c
ДикиЙ <islam7407@gmail.com>
2016-07-14 09:55:15 +0300

    Update

---
+++ config/steam-auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+     * Redirect URL after login
+     */
+    'redirect_url' => '/login',
+    /*
+     * API Key (http://steamcommunity.com/dev/apikey)
+     */
+    'api_key' => '9D56AA1135B684BC5ECAE13DC8038737',
+    /*
+     * Is using https ?
+     */
+    'https' => false
+
+];
